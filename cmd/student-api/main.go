f2846305f6bd30a7db7cbb5e9344958a032c9c06
Tukesh1 <tukeshkrraju1011@gmail.com>
2025-08-12 06:28:44 +0530

    gracefull shutdown

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,9 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/tukesh1/student-api/internal/config"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// log config
+	cfg := config.MustLoad()
+
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student-api"))
+	})
+
+
+
+	//setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	slog.Info("server started", slog.String("address", cfg.Addr))
+	// create a channel to store signal values
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	/*
+			os.Interrupt and syscall.SIGINT are both triggered by Ctrl+C in the terminal.
+		syscall.SIGTERM is a termination signal sent by the OS (e.g., when stopping a process).
+	*/
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done // till when there is no signal in done channel the server will run
+    slog.Info("sutting done the server")
+	ctx, cancel :=context.WithTimeout(context.Background(),5* time.Second)
+	defer cancel()
+	
+	if err:= server.Shutdown(ctx); err !=nil {
+		slog.Error("failed to shutdodn server", slog.String("error", err.Error()))
+	}
+	slog.Info("server shutdown successfully")
 }
